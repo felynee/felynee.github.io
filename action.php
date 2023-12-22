97363b1c7f700854a4f31440e0736f65560c13df
felyne <153820553+felynee@users.noreply.github.com>
2023-12-22 20:52:02 +0100

    Add files via upload

---
+++ action.php
@@ -0,0 +1,10 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $pseudo = $_POST["pseudo"]
+    {if (isset($_POST["espece"])){
+        $type = $_POST["espece"];
+        echo "Réponse séléctionnée : $espece"
+    }
+    }
+}
+?>
